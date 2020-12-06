f4759d1023bcde13da4fd6402adec2aa0624f7d9
Savan Raval <savan_raval@yahoo.com>
2020-12-06 23:09:37 +0530

    Changed index checking to set active class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 VehicleType.delete_all
-['Truck', 'Exotic', 'Vintage', 'Luxury', 'RV', 'SUV','Bike'].each do |ty|
+['Truck', 'Exotic', 'Vintage', 'Luxury', 'RV','Bike'].each do |ty|
     VehicleType.find_or_create_by(name: ty, active: true)
 end
